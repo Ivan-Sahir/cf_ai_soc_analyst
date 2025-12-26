39e8d77de2001c698529a2f1c468f5d117b635be
Ivan-Sahir <105741665+Ivan-Sahir@users.noreply.github.com>
2025-12-26 14:22:03 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,40 +1,40 @@
 export default {
   async fetch(request, env) {
-    //Allow the frontend to talk to worker (CORS)
-    const headers = {
+    const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "POST",
       "Access-Control-Allow-Headers": "Content-Type",
     };
 
-    //Handle the browser's security check
     if (request.method === "OPTIONS") {
-      return new Response(null, { headers });
+      return new Response( null, { headers: corsHeaders });
     }
 
-    //Get the log data from the user
-    if (request.method !== "POST") return new Response("Send a POST request", { headers });
-    
-    const { log_line } = await request.json();
 
-    //Ask the AI to analyze it
-    const aiResponse = await env.AI.run('@cf/meta/llama-3-8b-instruct', {
+    const data = await request.json();
+    const userLog = data.log_line; 
+
+    const aiResponse = await env.AI.run("@cf/meta/llama-3-8b-instruct", {
       messages: [
         { role: "system", content: "You are a SOC Analyst. Explain why this log is dangerous." },
-        { role: "user", content: log_line }
+        { role: "user", content: userLog }
       ]
     });
 
-    // Save the result to the Database (KV)
-    // current Time as the "Key" and the Analysis as the "Value"
-    await env.HISTORY.put(Date.now().toString(), JSON.stringify({
-        input: log_line,
-        result: aiResponse.response
+    const timeKey = Date.now().toString(); 
+    
+
+    await env.HISTORY.put(timeKey, JSON.stringify({
+        log: userLog,
+        analysis: aiResponse.response
     }));
 
-    // Send the answer back to the website
+
     return new Response(JSON.stringify(aiResponse), {
-      headers: { ...headers, "Content-Type": "application/json" }
+      headers: { 
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      }
     });
   }
 };
